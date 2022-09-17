fc7ab5fc1603ef2e88a5b521206d87c7f602cd94
fridayonojah <fridayonojah360@gmail.com>
2022-09-17 11:16:22 +0100

    finished setting up the confiq

--- startup/db.js
+++ startup/db.js
@@ -2,9 +2,18 @@ const winston = require('winston');
 const mongoose  = require('mongoose');
 const config = require('config');
 
+const connectionParams={
+  useNewUrlParser: true,
+  useUnifiedTopology: true 
+}
+
 module.exports = function (){
   const db = config.get('db');
   
-  mongoose.connect(db)
-  .then(() => console.log(`Connected to ${db} ...`));
+  mongoose.connect(db, connectionParams)
+  .then(() => console.log(`Connected to ${db} ...`))
+  .catch( (err) => {
+    console.error(`Error connecting to the database. n${err}`);
+  });
+
 }
